fb758da34bae6cd6b4e402dbfd62bd576ee77767
sravanmandava <sravanmandava8@gmail.com>
2021-04-28 03:22:15 +0530

    added allowed hosts

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'f#ix%j6wj$fg)2poly2f9(k)j6rc(7+5z=6lqvdz7uz^j4z5qz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-3-143-173-231.us-east-2.compute.amazonaws.com','127.0.0.1']
 
 
 # Application definition
