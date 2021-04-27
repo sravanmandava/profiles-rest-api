ede3e7369dc25e2496d47bfb087052417dcad785
sravanmandava <sravanmandava8@gmail.com>
2021-04-28 01:26:17 +0530

    added login API

--- proffiles_api/views.py
+++ proffiles_api/views.py
@@ -3,7 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status,viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
-
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from proffiles_api import serializers
 from proffiles_api import models
 from proffiles_api import permissions
@@ -81,3 +82,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name','email',)
+
+
+
+class UserLoginApiView(ObtainAuthToken):
+
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
